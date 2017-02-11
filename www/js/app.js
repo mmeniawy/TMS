3b9b003b63645346a45f7b518494a4c4ba6c0218
mmeniawy <mohamed.ielmeniawy@gmail.com>
2017-02-11 17:21:16 +0900

    work in progress

--- www/js/app.js
+++ www/js/app.js
@@ -1,17 +1,25 @@
 /// <script src="underscore.js"></script>
 (function(){
   'use strict';
+  document.addEventListener("deviceready", onDeviceReady, false);
+  function onDeviceReady() {
+    console.log(navigator.camera);
+  }
 
   var currentItem = {};
   var vehicles = [
         { vin:1010101010, color:"red", model:"nissan"},
         { vin:1010202020, color:"black", model:"fiat"},
-        { vin:1010203040, color:"black", model:"honda"}
-            
+        { vin:1010203040, color:"black", model:"honda"}            
+      ];
+      
+  var voyages = [
+        { eta:'10-02-2016 10:00',origin:'cairo',destination:'iraq'},
+        { eta:'11-02-2016 10:00',origin:'cairo2',destination:'iraq2'}
       ];
     
   var loadTrucksList = function(vList){
-        $('ons-list').html('');
+        $('#vehicles-list').html('');
         $.each(vList,function(index,element){
         var template = $($('#T')[0]).clone();
         $(template).prop('id',element.vin);
@@ -19,13 +27,52 @@
         $(template).find('ons-row > ons-col > header > span[tag="color"]').text(element.color);
         $(template).find('ons-row > ons-col > p[tag="model"]').text(element.model);
         $(template).removeClass('hidden');
-        $('ons-list').append(template);
+        $('#vehicles-list').append(template);
+    });
+  };
+  
+  var takePicture = function(){
+      navigator.camera.getPicture( cameraSuccess, cameraError, {
+        quality: 100,
+        destinationType: Camera.DestinationType.FILE_URI,
+        correctOrientation: true,
+        saveToPhotoAlbum: true
+    });
+  };
+  
+  var cameraSuccess = function(uri){
+      alert(uri);
+      takePicture();
+  };
+    var loadVoyages = function(vList){
+        
+        $('#voyages-list').html('');
+        $.each(vList,function(index,element){
+        var template = $($('#T1')[0]).clone();
+        $(template).prop('id',element.vin);
+        $(template).find('ons-row > ons-col > header > span[tag="ETA"]').text(element.eta);
+        $(template).find('ons-row > ons-col > p[tag="origin"]').text(element.origin);
+        $(template).find('ons-row > ons-col > p[tag="destination"]').text(element.destination);
+        $(template).removeClass('hidden');
+        $('#voyages-list').append(template);
     });
   };
   
   
+  $(document).on('click','#try-login',function(){
+        //some login logic in here
+        app.navi.pushPage('list.html');  
+  });
   
-  $(document).on('click','.item:not([tag="no-navigate"])',function(){
+  $(document).on('click','#take-picture',function(){
+      takePicture();
+  });
+  
+  $(document).on('click','.item[tag="voyage"]',function(){
+     app.navi.pushPage('list-vehicles.html');  
+  });
+    
+  $(document).on('click','.item[tag="vehicle"]:not([tag="no-navigate"])',function(){
     currentItem = {
         title : $('.item-title', this).text(),
         desc : $('.item-desc', this).text(),
@@ -47,7 +94,8 @@
   });
   $(document).on('click','#btn-save-report',function(){
     alert('report saved');
-    app.navi.popPage();
+    app.navi.pushPage('damage-report-photos.html');
+    //app.navi.popPage();
   });
   
   $(document).on('pageinit', '#detail-page', function() {
@@ -64,12 +112,18 @@
     $('.item-label', this).text(currentItem.label);
   });
 
-  $(document).on('pageinit', '#list-page', function() {
+  $(document).on('pageinit', '#list-vehicles-page', function() {
     
     loadTrucksList(vehicles);
         
   });
     
+  $(document).on('pageinit', '#list-page', function() {
+    
+    loadVoyages(voyages);
+        
+  });
+
   
   $(document).on('input','#filter-vehicles',function(){
       var filter = $('#filter-vehicles').val();
